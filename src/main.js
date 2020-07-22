223aa5120b8e99c8178d25a282034109dd6f8421
iqbal.muhammad <tapiiqbal@gmail.com>
2020-07-22 09:52:36 +0700

    edit pushManager resolve

--- src/main.js
+++ src/main.js
@@ -14,40 +14,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function requestPermission() {
-        if ('Notification' in window) {
-            Notification.requestPermission().then(function(result) {
-                if (result === "denied") {
-                    console.log("Fitur notifikasi tidak diijinkan.");
-                    return;
-                } else if (result === "default") {
-                    console.error("Pengguna menutup kotak dialog permintaan ijin.");
-                    return;
-                }
+        navigator.serviceWorker.ready.then(() => {
+            if ('Notification' in window) {
+                Notification.requestPermission().then(function(result) {
+                    if (result === "denied") {
+                        console.log("Fitur notifikasi tidak diijinkan.");
+                        return;
+                    } else if (result === "default") {
+                        console.error("Pengguna menutup kotak dialog permintaan ijin.");
+                        return;
+                    }
 
-                if (('PushManager' in window)) {
-                    navigator.serviceWorker.getRegistration().then(function(registration) {
-                        registration.pushManager.subscribe({
-                            userVisibleOnly: true,
-                            applicationServerKey: urlBase64ToUint8Array("BMxVCipfJYQOh7oP4pMtucmgViaxzfyEZDkgxADqfC6aa3ZRyXx9Sj_jCPcI0LQK37L9CgQezhsocbYElT_IKF8")
-                        }).then(function(subscribe) {
-                            console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
-                            console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
-                                null, new Uint8Array(subscribe.getKey('p256dh')))));
-                            console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
-                                null, new Uint8Array(subscribe.getKey('auth')))));
-                            localStorage.setItem('endpoint', JSON.stringify(subscribe.endpoint));
-                            localStorage.setItem('p256dh', JSON.stringify(btoa(String.fromCharCode.apply(
-                                null, new Uint8Array(subscribe.getKey('p256dh'))))));
-                            localStorage.setItem('auth', JSON.stringify(btoa(String.fromCharCode.apply(
-                                null, new Uint8Array(subscribe.getKey('auth'))))));
-                        }).catch(function(e) {
-                            location.reload();
-                            console.error(e.message);
+                    if (('PushManager' in window)) {
+                        navigator.serviceWorker.getRegistration().then(function(registration) {
+                            registration.pushManager.subscribe({
+                                userVisibleOnly: true,
+                                applicationServerKey: urlBase64ToUint8Array("BMxVCipfJYQOh7oP4pMtucmgViaxzfyEZDkgxADqfC6aa3ZRyXx9Sj_jCPcI0LQK37L9CgQezhsocbYElT_IKF8")
+                            }).then(function(subscribe) {
+                                console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+                                console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+                                    null, new Uint8Array(subscribe.getKey('p256dh')))));
+                                console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                                    null, new Uint8Array(subscribe.getKey('auth')))));
+                                localStorage.setItem('endpoint', JSON.stringify(subscribe.endpoint));
+                                localStorage.setItem('p256dh', JSON.stringify(btoa(String.fromCharCode.apply(
+                                    null, new Uint8Array(subscribe.getKey('p256dh'))))));
+                                localStorage.setItem('auth', JSON.stringify(btoa(String.fromCharCode.apply(
+                                    null, new Uint8Array(subscribe.getKey('auth'))))));
+                            }).catch(function(e) {
+                                console.error(e.message);
+                            });
                         });
-                    });
-                }
-            });
-        }
+                    }
+                });
+            }
+        })
     }
 
     function urlBase64ToUint8Array(base64String) {
