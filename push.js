19abe523d674c926873d1194a7ef28af88d15551
iqbal.muhammad <tapiiqbal@gmail.com>
2020-07-22 09:51:48 +0700

    edit variable type

--- push.js
+++ push.js
@@ -1,4 +1,4 @@
-var webPush = require('web-push');
+const webPush = require('web-push');
 const vapidKeys = {
     "publicKey": "BMxVCipfJYQOh7oP4pMtucmgViaxzfyEZDkgxADqfC6aa3ZRyXx9Sj_jCPcI0LQK37L9CgQezhsocbYElT_IKF8",
     "privateKey": "pr_XXwVplNF-hQ4adUqs2P4ImF46em4TMs_RjHW2sE8"
@@ -9,16 +9,16 @@ webPush.setVapidDetails(
     vapidKeys.publicKey,
     vapidKeys.privateKey
 )
-var pushSubscription = {
+const pushSubscription = {
     "endpoint": "https://fcm.googleapis.com/fcm/send/d6VUBJhkevs:APA91bGZQMYYIwhTDTUxw7JaTdJslD4xUuonhxbrVf6xPX9W4I_rO9yPyb2HXIP7O98cL0i4V0tmLCcFqey__t9LeeVLvc79Xb0moH77jW4DQwgbs6hC2bTHdPE5Rgjc_Ee1CLqJCMDs",
     "keys": {
         "p256dh": "BFvmXy3bkwYZzipYuq7CyIO3W8Yy2BdYZJlG4QPtN6LZEKT4fTbdb/nevH/JUZjhbaIdwQhW70QBTYP3a4ddhvg=",
         "auth": "7mOp/tUWNOesmss+i2/fkg=="
     }
 };
-var payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
+const payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
 
-var options = {
+const options = {
     gcmAPIKey: '849897549041',
     TTL: 60,
 };
